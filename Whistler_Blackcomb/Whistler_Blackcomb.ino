4d7fff75ff56bdc290edc9d5e38d7923f11a91a3
allans21 <samallan22@gmail.com>
2019-10-27 17:32:07 -0700

    initital ino

---
+++ Whistler_Blackcomb/Whistler_Blackcomb.ino
@@ -0,0 +1,10 @@
+//make sure to select teensy 4.0 for boards
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
