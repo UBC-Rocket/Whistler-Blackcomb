c525185619d3641d68caa0e81d77598fcde94b30
xander naumenko <xandernaumenko@gmail.com>
2019-11-03 15:09:50 -0800

    File organization

--- Whistler_Blackcomb/sensors.cpp
+++ Whistler_Blackcomb/sensors.cpp
@@ -6,6 +6,9 @@ Conatiains implementation of sensor initializations and data pulling
 
 /*Includes-----------------------------------------------------------------*/
 #include "includes\sensors.h"
+#include "includes\Adafruit_MCP9808.h"
+#include "includes\SparkFun_BNO080_Arduino_Library.h"
+#include "includes\SparkFun_LIS331.h"
 
 /*Constants----------------------------------------------------------------*/
 
