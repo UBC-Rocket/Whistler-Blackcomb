0fa700b96a0b6d9f67a6dcfbc5eda14c0006e842
allans21 <samallan22@gmail.com>
2019-11-02 17:22:47 -0700

    empty sensor files

---
+++ Whistler_Blackcomb/sensors.cpp
@@ -0,0 +1,13 @@
+/*
+Sensors Source
+
+Conatiains implementation of sensor initializations and data pulling
+*/
+
+/*Includes-----------------------------------------------------------------*/
+#include "sensors.h"
+
+/*Constants----------------------------------------------------------------*/
+
+/*Functions----------------------------------------------------------------*/
+
---
+++ Whistler_Blackcomb/sensors.h
@@ -0,0 +1,12 @@
+/*
+Sensors header
+
+File for functions of initializations and data retrieval from sensors
+*/
+
+/*Includes--------------------------------------------------------------------*/
+
+
+/*Functions-------------------------------------------------------------------*/
+
+
