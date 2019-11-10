53109d2933310cf7113fb1404d057357b0a825aa
Ben <b.huckell@hotmail.com>
2019-11-10 14:22:36 -0800

    working unit testing

--- Whistler_Blackcomb/test_functions.cpp
+++ Whistler_Blackcomb/test_functions.cpp
@@ -7,9 +7,9 @@ unsigned int Factorial( unsigned int number ) {
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(0) == 1 );
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+    CHECK( Factorial(0) == 1 );
+    CHECK( Factorial(1) == 1 );
+    CHECK( Factorial(2) == 2 );
+    CHECK( Factorial(3) == 6 );
+    CHECK( Factorial(10) == 3628800 );
 }
